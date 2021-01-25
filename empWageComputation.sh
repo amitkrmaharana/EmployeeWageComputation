a217926e03c0bc52cbf949867023351711e85066
amitkrmaharana <ak657harley@gmail.com>
2021-01-25 23:10:38 +0530

    Added Day along with daily wage and total wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+declare -A dailyWage
 isAbsent=0
 isFullday=1
 isParttime=2
@@ -37,11 +38,12 @@ do
 	emp_check=$((RANDOM%3))
 	daily_employee_wage="$( salary_per_day $emp_check)"
 	totalWage=$((totalWage+daily_employee_wage))
-	dailyWage[$total_working_days]=$daily_employee_wage
 	(( total_working_days++ ))
+	dailyWage["Day$total_working_days"]=$daily_employee_wage
 	total_working_hours="$( getWorkHours $emp_check )"
 done
 echo "Total wage of the employee in a month is $totalWage"
 echo "Total working days are $total_working_days"
 echo "Total working hours are $total_working_hours"
 echo "Daily wages of employee are as follows ${dailyWage[@]}"
+echo "Daily corresponding days are as follows ${!dailyWage[@]}"
