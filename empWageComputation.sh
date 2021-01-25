352277f0365ba36c54ab0becb757bca49bb1bf0b
amitkrmaharana <ak657harley@gmail.com>
2021-01-25 21:13:13 +0530

    Calculated Daily Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 isPresent=1
 isAbsent=0
+wage_per_hour=20
+full_day_hour=8
+daily_employee_wage=0
 echo "Welcome to Employee Wage Computation Program"
 emp_check=$((RANDOM%2))
 if [ $emp_check -eq $isPresent ]
 then
-	echo "Employee is Present"
-else
-	echo "Employee is Absent"
+	daily_employee_wage=$((wage_per_hour*full_day_hour))
 fi
+echo "Today's employee wage is $daily_employee_wage"
+
 
