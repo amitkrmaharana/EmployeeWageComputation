e274b0e031ab58b4c31a8578ad4f004767f19d4c
amitkrmaharana <ak657harley@gmail.com>
2021-01-25 22:37:59 +0530

    Condition of max working hours and days is applied

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -7,15 +7,27 @@ full_day_hour=8
 part_time_hour=8
 daily_employee_wage=0
 totalWage=0
+max_working_hours=100
+max_working_days=20
+total_working_hours=0
+total_working_days=0
 echo "Welcome to Employee Wage Computation Program"
-for((i=0;i<20;i++))
+while [ $total_working_hours -lt $max_working_hours ] && [ $total_working_days -lt $max_working_days ]
 do
 	emp_check=$((RANDOM%3))
 	case $emp_check in
-		$isFullday)daily_employee_wage=$((wage_per_hour*full_day_hour));;
-		$isParttime)daily_employee_wage=$((wage_per_hour*part_time_hour));;
-		$isAbsent)daily_employee_wage=0;;
+		$isFullday)
+			total_working_hours=$((total_working_hours+full_day_hour))
+			daily_employee_wage=$((wage_per_hour*full_day_hour));;
+		$isParttime)
+			total_working_hours=$((total_working_hours+part_time_hour))
+			daily_employee_wage=$((wage_per_hour*part_time_hour));;
+		$isAbsent)
+			daily_employee_wage=0;;
 	esac
 	totalWage=$((totalWage+daily_employee_wage))
+	(( total_working_days++ ))
 done
 echo "Total wage of the employee in a month is $totalWage"
+echo "Total working days are $total_working_days"
+echo "Total working hours are $total_working_hours"
