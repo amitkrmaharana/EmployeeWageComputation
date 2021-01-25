fac58bb19a06eb9cc25e4957b8b029fd7a2d35ba
amitkrmaharana <ak657harley@gmail.com>
2021-01-25 21:36:34 +0530

    Calculating wages for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,10 +6,16 @@ wage_per_hour=20
 full_day_hour=8
 part_time_hour=8
 daily_employee_wage=0
+totalWage=0
 echo "Welcome to Employee Wage Computation Program"
-emp_check=$((RANDOM%3))
-case $emp_check in
-	$isFullday)daily_employee_wage=$((wage_per_hour*full_day_hour));;
-	$isParttime)daily_employee_wage=$((wage_per_hour*part_time_hour));;
-esac
-echo "Today's employee wage is $daily_employee_wage"
+for((i=0;i<20;i++))
+do
+	emp_check=$((RANDOM%3))
+	case $emp_check in
+		$isFullday)daily_employee_wage=$((wage_per_hour*full_day_hour));;
+		$isParttime)daily_employee_wage=$((wage_per_hour*part_time_hour));;
+		$isAbsent)daily_employee_wage=0;;
+	esac
+	totalWage=$((totalWage+daily_employee_wage))
+done
+echo "Total wage of the employee in a month is $totalWage"
